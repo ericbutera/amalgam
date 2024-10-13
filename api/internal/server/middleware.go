4daa61366da0dee37d662886976e73221533d4d1
Eric Butera <eric@ericbutera.com>
2024-10-12 20:06:46 -0400

    refactor: middleware registration

--- api/internal/server/middleware.go
+++ api/internal/server/middleware.go
@@ -5,29 +5,49 @@ import (
 	"slices"
 	"time"
 
+	"github.com/ericbutera/amalgam/api/internal/config"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	sloggin "github.com/samber/slog-gin"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
 )
 
+var ignoredRoutes = []string{
+	"/metrics",
+	"/health",
+}
+
 func (s *server) middleware() {
-	ignoredRoutes := []string{"/metrics", "/health"}
-
 	s.router.Use(gin.Recovery())
-	s.router.Use(sloggin.NewWithFilters(slog.Default(), sloggin.IgnorePath(ignoredRoutes...)))
+	s.router.Use(logMiddleware())
+	s.router.Use(otelMiddleware())
+	s.router.Use(corsMiddleware(s.config))
+}
 
+func otelMiddleware() gin.HandlerFunc {
 	// otel: https://github.com/open-telemetry/opentelemetry-go-contrib/blob/main/instrumentation/github.com/gin-gonic/gin/otelgin/example/server.go
-	s.router.Use(otelgin.Middleware(MiddlewareName, otelgin.WithGinFilter(func(c *gin.Context) bool {
-		return !slices.Contains(ignoredRoutes, c.Request.URL.Path)
-	})))
+	return otelgin.Middleware(
+		MiddlewareName,
+		otelgin.WithGinFilter(func(c *gin.Context) bool {
+			return !slices.Contains(ignoredRoutes, c.Request.URL.Path)
+		}),
+	)
+}
 
-	s.router.Use(cors.New(cors.Config{
-		AllowOrigins:     s.config.CorsAllowOrigins,
-		AllowMethods:     s.config.CorsAllowMethods,
-		AllowHeaders:     s.config.CorsAllowHeaders,
-		ExposeHeaders:    s.config.CorsExposeHeaders,
+func corsMiddleware(cfg *config.Config) gin.HandlerFunc {
+	return cors.New(cors.Config{
+		AllowOrigins:     cfg.CorsAllowOrigins,
+		AllowMethods:     cfg.CorsAllowMethods,
+		AllowHeaders:     cfg.CorsAllowHeaders,
+		ExposeHeaders:    cfg.CorsExposeHeaders,
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
-	}))
+	})
+}
+
+func logMiddleware() gin.HandlerFunc {
+	return sloggin.NewWithFilters(
+		slog.Default(),
+		sloggin.IgnorePath(ignoredRoutes...),
+	)
 }
