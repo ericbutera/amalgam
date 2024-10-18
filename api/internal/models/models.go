8b1afcac2d7fc9ebf27a95b680d42a016642539e
Eric Butera <eric@ericbutera.com>
2024-10-18 07:38:22 -0400

    feat: basic validation

--- api/internal/models/models.go
+++ api/internal/models/models.go
@@ -10,15 +10,15 @@ import (
 
 type Feed struct {
 	Base
-	Url  string `json:"url" example:"https://example.com/"`
+	Url  string `json:"url" gorm:"uniqueIndex" binding:"required,url" example:"https://example.com/"`
 	Name string `json:"name" example:"Example"`
 }
 
 type Article struct {
 	Base
-	FeedID      uint   `json:"id" example:"1"`
+	FeedID      uint   `json:"feedId" binding:"required" example:"1"`
 	Feed        Feed   `gorm:"foreignKey:FeedID"`
-	Url         string `json:"url" example:"https://example.com/"`
+	Url         string `json:"url" binding:"required" example:"https://example.com/"`
 	Title       string `json:"title" example:"Example Article"`
 	ImageUrl    string `json:"imageUrl" example:"https://example.com/image.jpg"`
 	Preview     string `json:"preview" example:"Article preview text. May contain HTML."`
@@ -32,14 +32,14 @@ type User struct {
 	Base
 	UserUUID       string `json:"userUuid" example:"aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa"`
 	ProviderUserID string `json:"providerUserId" example:"eric"`
-	Username       string `json:"username" example:"eric"`
+	Username       string `json:"username" binding:"required" example:"eric"`
 	Name           string `json:"name" example:"Eric Butera"`
 	Email          string `json:"email" example:"example@example.com"`
 	PhotoURL       string `json:"photoUrl" example:"https://example.com/image.jpg"`
 }
 
 type Base struct {
-	ID        uint           `gorm:"primarykey" json:"id" example:"1"`
+	ID        uint           `gorm:"primarykey" json:"id" binding:"required" example:"1"`
 	CreatedAt time.Time      `json:"createdAt" example:"2021-01-01T00:00:00Z"`
 	UpdatedAt time.Time      `json:"updatedAt" example:"2021-01-01T00:00:00Z"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"deletedAt"`
