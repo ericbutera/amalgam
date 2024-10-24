c327ddf636cb78767c08e42528fae373a09adab1
Eric Butera <eric@ericbutera.com>
2024-10-24 10:36:31 -0400

    feat: trace correlation (#7)
    - add graph metrics
    - update docs
    - add observability doc with resources

--- graph/main.go
+++ graph/main.go
@@ -1,25 +1,51 @@
 package main
 
+//"github.com/99designs/gqlgen-contrib/prometheus"
 import (
+	"context"
+	"errors"
 	"log/slog"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	gql_prom "github.com/ericbutera/amalgam/graph/extensions/prometheus"
 	"github.com/ericbutera/amalgam/graph/graph"
 	"github.com/ericbutera/amalgam/graph/internal/config"
 	cfg "github.com/ericbutera/amalgam/pkg/config"
+	"github.com/ericbutera/amalgam/pkg/otel"
 	rpc "github.com/ericbutera/amalgam/rpc/pkg/client"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/ravilushqa/otelgqlgen"
 )
 
 func main() {
+	h := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug})
+	slog.SetDefault(slog.New(h))
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
 	cfg, err := cfg.NewFromEnv[config.Config]()
 	if err != nil {
 		slog.Error("failed to load config", "error", err)
 		os.Exit(1)
 		return
 	}
+
+	shutdown, err := otel.Setup(ctx)
+	if err != nil {
+		slog.Error("failed to setup otel", "error", err)
+		os.Exit(1)
+	}
+	defer func() {
+		err = errors.Join(err, shutdown(context.Background()))
+	}()
+
+	gql_prom.Register()
 
 	c, err := rpc.NewClient(cfg.RpcHost)
 	if err != nil {
@@ -29,12 +55,20 @@ func main() {
 	defer c.Conn.Close()
 
 	apiClient := graph.NewApiClient(cfg.ApiScheme, cfg.ApiHost)
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{
-		Resolvers: graph.NewResolver(cfg, apiClient, c.Client),
-	}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	srv := handler.NewDefaultServer(
+		graph.NewExecutableSchema(graph.Config{
+			Resolvers: graph.NewResolver(cfg, apiClient, c.Client),
+		}),
+	)
+
+	srv.Use(otelgqlgen.Middleware())
+	srv.Use(gql_prom.Tracer{})
+	// TODO: complexity limit srv.Use(extension.ComplexityLimit{})
+
+	http.Handle("/metrics", promhttp.Handler())
 	http.Handle("/query", srv)
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 
 	slog.Info("running graphql playground", "port", cfg.Port)
 	if err := http.ListenAndServe(":"+cfg.Port, nil); err != nil {
--- lgtm/README.md
+++ lgtm/README.md
@@ -1,3 +1,17 @@
 # LGTM Stack
 
 This uses a LGTM demonstration image. I then override a few configurations to make it work with the tilt environment.
+
+## Add a Dashboard
+
+1. add dashboard json file to this directory
+2. modify Dockerfile to `COPY <dashboard> /otel-lgtm/<dashboard.json>`
+3. modify `grafana-dashboards.yaml` to include the dashboard
+
+```yaml
+  - name: "Amalgam Dashboard"
+    type: file
+    options:
+      path: /otel-lgtm/amalgam-dashboard.json
+      foldersFromFilesStructure: false
+```
