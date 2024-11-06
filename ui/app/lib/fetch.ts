5035588c8f8c0089b168444d98a9fbe5b82f4d8e
Eric Butera <eric@ericbutera.com>
2024-11-06 13:10:12 -0500

    feat(ui): graphql (#38)
    * feat(ui): use graphql client
    
    * ops: refactor graph
    
    * chore: remove old api client
    
    * chore: remove api references
    
    * ops: ts generate graphql client
    
    * chore: cleanup
    
    * fix: feeds routing
    
    corrected /feed/:id/articles -> /feeds/:id/articles.

--- ui/app/lib/fetch.ts
+++ ui/app/lib/fetch.ts
@@ -1,4 +1,15 @@
-import { DefaultApi } from '../lib/client/apis/DefaultApi';
+// TODO: use tanstack/react-query
+import { GraphQLClient } from 'graphql-request';
+import { getSdk } from '../generated/graphql';
+
+export const getGraph = (url: string) => {
+  if (!process.env.NEXT_PUBLIC_GRAPHQL_API_URL) {
+    throw new Error('NEXT_PUBLIC_GRAPHQL_API_URL is not defined');
+  }
+  // TODO: should this be a singleton?
+  const endpoint = url || process.env.NEXT_PUBLIC_GRAPHQL_API_URL;
+  return getSdk(new GraphQLClient(endpoint));
+}
 
 export default async function fetcher<JSON = any>(
   input: RequestInfo,
@@ -7,6 +18,3 @@ export default async function fetcher<JSON = any>(
   const res = await fetch(input, init)
   return res.json()
 }
-
-// TODO: DefaultApi is probably the wrong usage of this; needs more research
-export const getApi = () => new DefaultApi();
