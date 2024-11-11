9212e794dadaec99a378da0b931a472cc7b35d9e
Eric Butera <eric@ericbutera.com>
2024-11-10 20:23:25 -0500

    test: feed save activity (#49)

--- data-pipeline/temporal/feed/activity.go
+++ data-pipeline/temporal/feed/activity.go
@@ -41,11 +41,15 @@ func NewActivities(transforms transforms.Transforms, fetch fetch.Fetch, bucket b
 	}
 }
 
+func RssPath(feedId string) string {
+	return fmt.Sprintf(RssPathFormat, feedId)
+}
+
 // Download RSS feeds.
 func (a *Activities) DownloadActivity(ctx context.Context, feedId string, url string) (string, error) {
 	// TODO: research temporal metrics to see if there is a built in way to view how long "downloads" are taking
 	// TODO: also research to get counts of how many downloads are happening
-	rssFile := fmt.Sprintf(RssPathFormat, feedId)
+	rssFile := RssPath(feedId)
 	entry := slog.Default().With(
 		"feed_id", feedId,
 		"rss_file", rssFile,
@@ -65,10 +69,14 @@ func (a *Activities) DownloadActivity(ctx context.Context, feedId string, url st
 	return rssFile, nil
 }
 
+func ArticlePath(feedId string) string {
+	return fmt.Sprintf(ArticlePathFormat, feedId)
+}
+
 // Transform raw rss file into structured articles.
 // TODO: actually support a streaming bucket read -> rss to articles -> bucket write
 func (a *Activities) ParseActivity(ctx context.Context, feedId string, rssFile string) (string, error) { // TODO: ParseActivity -> ArticleActivity
-	articlesFile := fmt.Sprintf(ArticlePathFormat, feedId)
+	articlesFile := ArticlePath(feedId)
 	entry := slog.Default().With(
 		"feed_id", feedId,
 		"article_file", articlesFile,
@@ -87,7 +95,6 @@ func (a *Activities) ParseActivity(ctx context.Context, feedId string, rssFile s
 
 	jsonl, errs := a.transforms.ArticleToJsonl(feedId, articles)
 	if len(errs) > 0 {
-		entry.Error("parse activity: encode errors", "errors", len(errs))
 		for err := range errs {
 			entry.Debug("article to jsonlines", "error", err)
 		}
@@ -95,7 +102,6 @@ func (a *Activities) ParseActivity(ctx context.Context, feedId string, rssFile s
 
 	upload, err := a.bucket.WriteStream(ctx, BucketName, articlesFile, &jsonl, ArticleContentType)
 	if err != nil {
-		entry.Error("parse activity: write error", "error", err)
 		return articlesFile, err
 	}
 	entry.Debug("parse activity: upload info", "key", upload.Key, "bucket", upload.Bucket, "size", upload.Size)
@@ -108,55 +114,35 @@ type SaveResults struct {
 }
 
 // Load articles into database.
-func (a *Activities) SaveActivity(ctx context.Context, feedId string, articlesPath string) error {
+func (a *Activities) SaveActivity(ctx context.Context, feedId string, articlesPath string) (SaveResults, error) {
 	entry := slog.Default().With(
 		"feed_id", feedId,
 	)
+	results := SaveResults{}
 
 	articleReader, err := a.bucket.Read(ctx, BucketName, articlesPath)
 	if err != nil {
-		entry.Error("save: read error", "file", articlesPath, "error", err)
-		return err
+		return results, err
 	}
-
-	results := SaveResults{}
 
 	defer articleReader.Close()
 	decoder := json.NewDecoder(articleReader)
 
-	// TODO: refactor to use a batch upsert
-	// TODO: reduce branch complexity
 	for {
 		var article parse.Article
 		if err := decoder.Decode(&article); err != nil {
-			if err == context.Canceled {
-				entry.Error("save: context canceled")
-				results.Failed++
-				continue // return nil
-			}
-			if err == context.DeadlineExceeded {
-				entry.Error("save: context deadline exceeded")
-				continue // return nil
-			}
 			if err == io.EOF {
 				break
 			}
 
 			results.Failed++
 			entry.Error("save: decode error", "error", err)
-			return err
+			continue
 		}
 
-		id, err := a.feeds.SaveArticle(ctx, article) // TODO: ensure idempotent upsert
+		id, err := a.feeds.SaveArticle(ctx, article)
 		if err != nil {
-			code := status.Code(err)
-			if code == codes.InvalidArgument {
-				for _, detail := range status.Convert(err).Details() {
-					entry.Error("save article", "article_url", article.Url, "error", detail)
-				}
-			}
-
-			entry.Error("save article", "article_url", article.Url, "error", err)
+			handleSaveError(err, article.Url, entry)
 			results.Failed++
 			continue
 		}
@@ -165,6 +151,21 @@ func (a *Activities) SaveActivity(ctx context.Context, feedId string, articlesPa
 		entry.Debug("saved article", "article_url", article.Url, "article_id", id)
 	}
 
-	entry.Debug("save results", "succeeded", results.Succeeded, "failed", results.Failed)
-	return nil
+	entry.Info("save results", "succeeded", results.Succeeded, "failed", results.Failed)
+
+	// TODO: increment failure counter for alerting (does temporal have built in metrics for this?)
+	return results, nil
+}
+
+func handleSaveError(err error, url string, entry *slog.Logger) {
+	// TODO: research recording errors like this using grpc middleware
+	// need to have to correlate errors with the feed & article that caused it
+	code := status.Code(err)
+	if code == codes.InvalidArgument {
+		for _, detail := range status.Convert(err).Details() {
+			entry.Error("save article", "article_url", url, "error", detail)
+		}
+		return
+	}
+	entry.Error("save article", "article_url", url, "error", err)
 }
