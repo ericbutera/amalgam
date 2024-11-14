b28e51b3197866426ea286fb85f4e98f620f266f
Eric Butera <eric@ericbutera.com>
2024-11-13 20:26:42 -0500

    feat: grafana alerting
    I did not want to add any external services or add more things like httpbin to show webhooks in this project. In the real world these would go to slack or pagerduty.
    
    Changes:
    
    - create fake "echo-webhook" service to print incoming request
    - copy alert yaml files into lgtm image
    - create feed workflow alert on failed workflows
    - alerts go to "echo-webhook" service

---
+++ services/echo/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"net/http"
+
+	"github.com/ericbutera/amalgam/internal/http/server"
+	"github.com/samber/lo"
+)
+
+func webhookHandler(w http.ResponseWriter, r *http.Request) {
+	slog.Info("Received webhook",
+		"method", r.Method,
+		"url", r.URL,
+		"remote_addr", r.RemoteAddr,
+		"user_agent", r.UserAgent(),
+		"content_length", r.ContentLength,
+	)
+
+	var payload map[string]any
+	err := json.NewDecoder(r.Body).Decode(&payload)
+	if err != nil {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+
+	data, err := json.MarshalIndent(payload, "", "  ")
+	if err != nil {
+		slog.Error("Failed to marshal payload", "error", err)
+		http.Error(w, "Error processing request", http.StatusInternalServerError)
+		return
+	}
+	lo.Must(fmt.Printf("headers: %+v\n", r.Header))
+	lo.Must(fmt.Printf("payload: %s\n", string(data)))
+
+	w.WriteHeader(http.StatusOK)
+	if _, err := fmt.Fprintln(w, "Alert received"); err != nil {
+		slog.Error("Failed to write response", "error", err)
+	}
+}
+
+func main() {
+	http.HandleFunc("/webhook", webhookHandler)
+
+	port := "8080"
+	slog.Info("Webhook receiver is running", "port", port)
+
+	server := lo.Must(server.New())
+	err := server.ListenAndServe()
+	if err != nil {
+		slog.Error("Failed to start server", "error", err)
+	}
+}
