9eee261c7ac84db454b60666570aad864623a985
Eric Butera <eric@ericbutera.com>
2024-11-17 15:22:09 -0500

    refactor: graph validation

---
+++ integration/graph_test.go
@@ -0,0 +1,58 @@
+//go:build integration
+// +build integration
+
+package integration_test
+
+// Graph is the entrypoint for the entire system. It is the best place to build out coverage.
+
+import (
+	"context"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/Khan/genqlient/graphql"
+	graphClient "github.com/ericbutera/amalgam/pkg/clients/graphql"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// TODO: ability to seed specific data for testing
+// TODO: truncate between tests
+
+func TestGraphListFeeds(t *testing.T) {
+	res, err := graphClient.ListFeeds(context.Background(), getGraphQLClient(t))
+	require.NoError(t, err)
+	assert.Empty(t, res.Feeds)
+}
+
+func TestGraphGetFeedMissingID(t *testing.T) {
+	_, err := graphClient.GetFeed(context.Background(), getGraphQLClient(t), "uid")
+	require.ErrorContains(t, err, "value must be a valid UUID")
+}
+
+func TestGraphGetFeedNotFound(t *testing.T) {
+	_, err := graphClient.GetFeed(context.Background(), getGraphQLClient(t), "e97f8e74-1183-4280-a48d-dd592e013ee1")
+	require.ErrorContains(t, err, "feed not found")
+}
+
+func TestListArticlesMissingID(t *testing.T) {
+	_, err := graphClient.ListArticles(context.Background(), getGraphQLClient(t), "uid")
+	require.Error(t, err)
+}
+
+func TestGraphGetArticleMissingID(t *testing.T) {
+	_, err := graphClient.GetArticle(context.Background(), getGraphQLClient(t), "")
+	require.Error(t, err)
+}
+
+func getGraphQLClient(t *testing.T) graphql.Client {
+	target := os.Getenv("GRAPH_HOST")
+	require.NotEmpty(t, target, "GRAPH_HOST not set")
+
+	httpClient := http.Client{}
+	return graphql.NewClient(
+		target,
+		&httpClient,
+	)
+}
