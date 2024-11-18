00984af601201699d131e9a620c27724ef99c8bd
Eric Butera <eric@ericbutera.com>
2024-11-17 21:04:30 -0500

    fix: ui dev query graph (#60)

--- ui/app/lib/fetch.ts
+++ ui/app/lib/fetch.ts
@@ -3,10 +3,13 @@ import { GraphQLClient } from 'graphql-request';
 import { getSdk } from '../generated/graphql';
 
 export const getGraph = (url: string) => {
-  if (!process.env.NEXT_PUBLIC_GRAPHQL_API_URL) {
-    throw new Error('NEXT_PUBLIC_GRAPHQL_API_URL is not defined');
-  }
   // TODO: should this be a singleton?
   const endpoint = url || process.env.NEXT_PUBLIC_GRAPHQL_API_URL;
+
+  if (!endpoint) {
+    console.error("NEXT_PUBLIC_GRAPHQL_API_URL is not defined");
+    throw new Error('NEXT_PUBLIC_GRAPHQL_API_URL is not defined');
+  }
+
   return getSdk(new GraphQLClient(endpoint));
 }
