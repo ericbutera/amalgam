41c7b78dcbcb93607a45547777b4e25a4051fed3
Eric Butera <eric@ericbutera.com>
2024-11-26 13:23:09 -0500

    fix: response data is nested

--- ui/app/data/articles.ts
+++ ui/app/data/articles.ts
@@ -2,7 +2,7 @@ import useSWR from "swr";
 import { getGraph } from '../lib/fetch';
 
 export default function useArticles(id: string) {
-  const fetcher = async () => await getGraph().ListArticles({feedId: id})
+  const fetcher = async () => await getGraph().ListArticles({ feedId: id })
   const { data, mutate, error } = useSWR(`/feeds/${id}/articles`, fetcher);
   const loading = !data && !error;
 
