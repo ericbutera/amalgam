c1acad5697e3e3794a78d4b1d02add62fc127b66
Eric Butera <eric@ericbutera.com>
2024-11-26 10:00:33 -0500

    feat: article header

---
+++ ui/app/data/feed.ts
@@ -0,0 +1,17 @@
+import useSWR from "swr";
+import { getGraph } from "../lib/fetch";
+
+export default function useFeed(id: string) {
+  const fetcher = async () => {
+    return await getGraph().GetFeed({ id });
+  };
+  const { data, mutate, error } = useSWR(id ? `/feed/${id}` : null, fetcher);
+  const loading = !data && !error;
+
+  return {
+    loading,
+    error,
+    feed: data?.feed,
+    mutate,
+  };
+}
