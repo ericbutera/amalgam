ad5454b6ecbffce79e03714a8a04e8484607bd5f
Eric Butera <eric@ericbutera.com>
2024-12-02 20:05:17 -0500

    refactor: move mark read into page

--- ui/app/data/article.ts
+++ ui/app/data/article.ts
@@ -1,20 +1,8 @@
 import useSWR from "swr";
 import { getGraph } from "../lib/fetch";
-import toast from "react-hot-toast";
 
 export default function useArticle(id: string) {
-  const fetcher = async () => {
-    const graph = getGraph();
-    const article = await graph.GetArticle({ id });
-
-    if (article && !article.article?.userArticle?.viewedAt) {
-      graph.MarkArticleRead({ id }).catch(() => {
-        toast.error("Failed to mark article as read");
-      });
-    }
-
-    return article;
-  };
+  const fetcher = async () => await getGraph().GetArticle({ id });
   const { data, mutate, error } = useSWR(`/article/${id}`, fetcher);
   const loading = !data && !error;
 
